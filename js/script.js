571be016ab04b5224b49b28e622daa62278c486c
fitton2024 <fitton2024@gmail.com>
2024-12-08 02:04:17 +0900

    lessons

--- js/script.js
+++ js/script.js
@@ -33,3 +33,15 @@ $(function () {
     }
   });
 });
+
+$(function() {
+  $('[name="q1"]:radio').change( function() {
+    if($('[id=a]').prop('checked')){
+      $('.text').fadeOut();
+      $('.text01').fadeIn();
+    } else if ($('[id=b]').prop('checked')) {
+      $('.text').fadeOut();
+      $('.text02').fadeIn();
+    } 
+  });
+});
